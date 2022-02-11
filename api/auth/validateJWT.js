4056c799163430b6208290bebb3332c5381b3561
Paulo França <paulovinicius.franca@gmail.com>
2022-02-11 19:31:10 -0300

    Requisito 3

--- api/auth/validateJWT.js
+++ api/auth/validateJWT.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../../models');
+// const { User } = require('../../models');
 
 const segredo = process.env.JWT_SECRET;
 
@@ -13,18 +13,22 @@ module.exports = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, segredo);
 
-    const user = await User.findOne({ where: { username: decoded.data.username } });
+    console.log(decoded);
 
-    if (!user) {
-      return res
-        .status(401)
-        .json({ message: 'Erro ao procurar usuário do token.' });
-    }
+    // if (!decoded) {
+    //   return res.status(401).json({ message: 'Expired or invalid token.' });
+    // }
 
-    req.user = user;
+    // const user = await User.findOne({ where: { email: decoded.data.email } });
+
+    // if (!user) {
+    //   return res.status(401).json({ message: 'Erro ao procurar usuário do token.' });
+    // }
+
+    // req.user = user;
 
     next();
   } catch (err) {
-    return res.status(401).json({ message: err.message });
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
